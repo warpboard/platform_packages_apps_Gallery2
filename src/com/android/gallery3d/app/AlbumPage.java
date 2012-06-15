d8d34dd1d0a53042317e0f38baf714341ea6ecd8
Yuriy Zabroda <yuriy.zabroda@ti.com>
2012-06-15 11:12:35 +0300

    Ensure that proper video details are shown in Gallery
    This change fixes the following bug:
    
    1. Go to Gallery App and select a folder with more than 3 videos.
    2. Tap and hold one of the videos until it is highlighted.
    3. Continue selecting the first 3 videos.
    4. Tap and hold the videos in the opposite order now, only one video should remain highlighted.
    5. Tap the menu button at the top right corner and select details.
    6. The details for the last unselected video are shown instead of the highlighted video.
    
    Change-Id: I3722750a3cde3a6868d517dbc446387897213e03
    Signed-off-by: Yuriy Zabroda <yuriy.zabroda@ti.com>

--- src/com/android/gallery3d/app/AlbumPage.java
+++ src/com/android/gallery3d/app/AlbumPage.java
@@ -56,6 +56,7 @@ import com.android.gallery3d.ui.StaticBackground;
 import com.android.gallery3d.util.Future;
 import com.android.gallery3d.util.GalleryUtils;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class AlbumPage extends ActivityState implements GalleryActionBar.ClusterRunner,
@@ -225,6 +226,19 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
             if (item == null) return;
             mSelectionManager.setAutoLeaveSelectionMode(true);
             mSelectionManager.toggle(item.getPath());
+
+            // We need to handle a specific use case when several album items
+            // were selected / unselected (perhaps many times), and finally
+            // only one album item is left selected.
+            //
+            // Note: actually selected item is not necessarily the one
+            // identified by "slotIndex" argument, because an item could also
+            // be unselected by long press, not only be selected.
+            ArrayList<Path> selectedPaths = mSelectionManager.getSelected(false);
+            if (selectedPaths.size() == 1) {
+                slotIndex = mSelectionManager.getSourceMediaSet().getIndexOfItem(selectedPaths.get(0), 0);
+            }
+
             mDetailsSource.findIndex(slotIndex);
             mAlbumView.invalidate();
         }
